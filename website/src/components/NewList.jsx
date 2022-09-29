d16836d982f5d092c6c171609b7f2ceeab0ec618
Jun Kim <64379343+junkim012@users.noreply.github.com>
2022-09-29 10:55:06 -0500

    deletes right item and no more dnd error

--- website/src/components/NewList.jsx
+++ website/src/components/NewList.jsx
@@ -6,21 +6,21 @@ import Block from './Block';
 
 
 const grid = 8;
-const getItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
-    userSelect: "none",
-    padding: grid * 2,
-    margin: `0 0 ${grid}px 0`,
-  
-    // change background colour if dragging
-    background: isDragging ? "lightgreen" : "grey",
-  
-    // styles we need to apply on draggables
-    ...draggableStyle
-  });
+// const getItemStyle = (isDragging, draggableStyle) => ({
+//     // some basic styles to make the items look a bit nicer
+//     userSelect: "none",
+//     padding: grid * 2,
+//     margin: `0 0 ${grid}px 0`,
 
-export default function NewList({ blocks, removeBlock }) {
-    console.log('blocks: ', blocks); 
+//     // change background colour if dragging
+//     background: isDragging ? "lightgreen" : "grey",
+
+//     // styles we need to apply on draggables
+//     ...draggableStyle
+//   });
+
+export default function NewList({ blocks, setBlocks, removeBlock }) {
+    console.log('blocks: ', blocks);
     const [toPrint, setToPrint] = useState("hello");
 
     const reorder = (list, startIndex, endIndex) => {
@@ -33,53 +33,56 @@ export default function NewList({ blocks, removeBlock }) {
     function onDragEnd(result) {
         // dropped outside the list
         if (!result.destination) {
-          return;
+            return;
         }
-    
-        const items = reorder(
-          this.state.items,
-          result.source.index,
-          result.destination.index
+
+        const reOrdered = reorder(
+            blocks,
+            result.source.index,
+            result.destination.index
         );
-    
-        this.setState({
-          items
-        });
-      }
+
+        setBlocks(reOrdered);
+    }
 
     return (
         <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="droppableee">
                 {(provided, snapshot) => (
                     <div
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
                     >
-                        {
-                            blocks.map((block, idx) => {
-                                
-                                console.log('block.id', block.id); 
-                                return (
-                                    <Draggable key={idx} draggableId={block.id} index={idx}>
-                                        {(provided, snapshot) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                                style={getItemStyle(
-                                                    snapshot.isDragging,
-                                                    provided.draggableProps.style
-                                                  )}
-                                            >
-                                                {idx}
-                                                <Block key={idx} defaultName={block.defaultName} defaultChain={block.defaultChain} index={idx} removeBlock={removeBlock}></Block>
-                                            </div>
-                                        )}
-                                    
-                                    </Draggable>
-                                )
-                            })
-                        }
+                        <div>
+
+                            {
+                                blocks.map((block, idx) => {
+
+                                    console.log('block.id', block.id);
+                                    console.log('block index: ', idx); 
+                                    return (
+                                        <Draggable key={block.id} draggableId={block.id} index={idx}>
+                                            {(provided, snapshot) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                // style={getItemStyle(
+                                                //     snapshot.isDragging,
+                                                //     provided.draggableProps.style
+                                                //   )}
+                                                >
+                                                    {block.id}
+                                                    <Block key={idx} defaultName={block.defaultName} defaultChain={block.defaultChain} index={idx} removeBlock={removeBlock}></Block>
+                                                </div>
+                                            )}
+
+                                        </Draggable>
+                                    )
+                                })
+                            }
+                            {provided.placeholder}
+                        </div>
 
                     </div>
                 )}
